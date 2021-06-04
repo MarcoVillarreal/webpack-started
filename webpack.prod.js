3f7f47293354bf75e529155f9ee0ada484af70ac
Marco <marco.villarreal.jacobo@gmail.com>
2021-06-04 12:32:04 -0500

    primer comit

---
+++ webpack.prod.js
@@ -0,0 +1,67 @@
+const HtmlWebPack = require('html-webpack-plugin');
+const MiniCssExtract = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizer = require('css-minimizer-webpack-plugin');
+const Terser = require('terser-webpack-plugin');
+
+module.exports = {
+
+    mode:'production',
+
+    output:{
+        clean: true
+    },
+
+    module:{
+        rules:[
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options:{
+                    sources: false,
+                },
+            
+
+            },
+            {
+                test: /\.css$/,
+                exclude: /style.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test: /style.css$/,
+                use: [MiniCssExtract.loader,'css-loader']
+
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
+            }
+        
+        ]
+    },
+
+    optimization:{
+        minimize:true,
+        minimizer:[
+            new CssMinimizer(),
+            new Terser(),
+        ]
+    },
+    plugins:[
+        new HtmlWebPack({
+            title:'Mi webpack app',
+            //filename: 'index.html'
+            template:'./src/index.html'
+        }),
+
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder:false
+        }),
+
+        new CopyPlugin({
+            patterns:[{from:'src/assets/',to:'assets/'}]
+        }),
+    ]
+}
